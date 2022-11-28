530483b3633800ccaa193cf0deb57f465a7d96c0
Asandile <asandilelangeni@gmail.com>
2022-11-28 11:29:47 +0200

    first commit

---
+++ js/code.js
@@ -0,0 +1,10 @@
+// (fetch('../data/data.json'))
+// .then((response)=> {
+//     return response.json()
+// })
+// .then((data)=> {
+//     console.table(data.results);
+// })
+// .catch(()=> {
+//     console.log("Please try again later");
+// });
